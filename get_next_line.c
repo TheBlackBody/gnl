f9bb1fa5b94de81cf6d0208ab2f9b806a12dbb74
TheBlackBody <66089114+TheBlackBody@users.noreply.github.com>
2023-01-02 17:28:36 +0100

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 15:45:05 by sfernand          #+#    #+#             */
-/*   Updated: 2023/01/02 17:05:10 by sfernand         ###   ########.fr       */
+/*   Updated: 2023/01/02 17:28:25 by sfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -18,6 +18,8 @@ char	*rad_file(int fd, char *res_buff)
 
 	if (!res_buff)
 		res_buff = ft_calloc(1, 1);
+	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	n_read = 1;
 }
 
 char *get_next_line(int fd)
