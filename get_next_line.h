e98244f5bab41e8748b454ca753ed5cc16d380d5
TheBlackBody <66089114+TheBlackBody@users.noreply.github.com>
2023-01-02 17:04:14 +0100

    get_next_line

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: sfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 15:34:51 by sfernand          #+#    #+#             */
-/*   Updated: 2023/01/02 16:03:08 by sfernand         ###   ########.fr       */
+/*   Updated: 2023/01/02 17:04:07 by sfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,9 @@
 # include <stdarg.h>
 # include <unistd.h>
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
+
+void	ft_bzero(void *p, size_t n);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
