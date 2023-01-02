29db45c0aa3301f449e3a7b26ec6901f40eba7cf
TheBlackBody <66089114+TheBlackBody@users.noreply.github.com>
2023-01-02 16:57:55 +0100

    get_next_line

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,38 @@
 /*   By: sfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 16:21:51 by sfernand          #+#    #+#             */
-/*   Updated: 2022/12/20 16:21:53 by sfernand         ###   ########.fr       */
+/*   Updated: 2023/01/02 16:57:49 by sfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "get_next_line.h"
 
+void	ft_bzero(void *p, size_t n)
+{
+	size_t	i;
+	char	*a;
 
+	i = 0;
+	a = (char *)p;
+	while (i < n)
+	{
+		a[i] = 0;
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char		*dest;
+	size_t		n;
+
+	n = (count * size);
+	if (count != 0 && size >= SIZE_MAX / count)
+		return (0);
+	dest = (void *)malloc(n);
+	if (!dest)
+	{
+		return (NULL);
+	}
+	ft_bzero(dest, n);
+	return (dest);
+}
