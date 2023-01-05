8b1d1fd01f430f99a808b9f6bc720f8f16de010c
TheBlackBody <66089114+TheBlackBody@users.noreply.github.com>
2023-01-05 09:52:19 +0100

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,65 @@
 /*   By: sfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/13 15:45:05 by sfernand          #+#    #+#             */
-/*   Updated: 2023/01/03 17:38:00 by sfernand         ###   ########.fr       */
+/*   Updated: 2023/01/04 17:41:05 by sfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
 char	*join(char *res, char *buffer)
 {
-	char *temp;
+	char	*temp;
 
 	temp = ft_strjoin(res, buffer);
-	free(buffer);
 	return (temp);
 }
 
-char	*rad_file(int fd, char *res_buff)
+char	*put_next_line(char *buff)
+{
+	char	*next;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (buff[i] && buff[i] != '\n')
+		i++;
+	if (!buff[i])
+	{
+		free(buff);
+		return (NULL);
+	}
+	next = ft_calloc((ft_strlen(buff) - i + 1), sizeof(char));
+	i++;
+	j = 0;
+	while (buff[i])
+		next[j++] = buff[i++];
+	free (buff);
+	return (next);
+}
+
+char	*put_line(char *buff)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!buff[i])
+		return (NULL);
+	while (buff[i] && buff[i] != '\n')
+		i++;
+	line = ft_calloc(i + 2, sizeof(char));
+	i = 0;
+	while (buff[i] && buff[i] != '\n')
+	{
+		line[i] = buff[i];
+		i++;
+	}
+	if (buff[i] && buff[i] == '\n')
+		line[i++] = '\n';
+	return (line);
+}
+
+char	*read_file(int fd, char *res_buff)
 {
 	char	*buffer;
 	int		n_read;
@@ -32,7 +76,7 @@ char	*rad_file(int fd, char *res_buff)
 	while (n_read > 0)
 	{
 		n_read = read(fd, buffer, BUFFER_SIZE);
-		if (n_read == 0)
+		if (n_read == -1)
 		{
 			free (buffer);
 			return (NULL);
@@ -40,7 +84,7 @@ char	*rad_file(int fd, char *res_buff)
 		buffer[n_read] = 0;
 		res_buff = join(res_buff, buffer);
 		if (ft_strchr(buffer, '\n'))
-			break;
+			break ;
 	}
 	free (buffer);
 	return (res_buff);
@@ -51,7 +95,12 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char		*line;
 
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+		return (NULL);
 	buffer = read_file(fd, buffer);
 	if (!buffer)
 		return (NULL);
+	line = put_line(buffer);
+	buffer = put_next_line(buffer);
+	return (line);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,10 +6,66 @@
 /*   By: sfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 16:21:51 by sfernand          #+#    #+#             */
-/*   Updated: 2023/01/02 16:57:49 by sfernand         ###   ########.fr       */
+/*   Updated: 2023/01/04 16:20:47 by sfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char *a)
+{
+	int	i;
+
+	i = 0;
+	while (a[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*dest;
+	int		i;
+	int		n;
+	int		len1;
+	int		len2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	i = 0;
+	dest = malloc(len1 + len2 + 1);
+	if (!dest)
+		return (0);
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	n = 0;
+	while (s2[n])
+		dest[i++] = s2[n++];
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
+	return (0);
+}
 
 void	ft_bzero(void *p, size_t n)
 {
